5eb466df104d75ee60d27d76baffbc8bc2d8b063
Hiroki Noda <kubo39@gmail.com>
2020-02-04 23:38:43 +0900

    Improve get total RAM

--- source/systeminfo/memory.d
+++ source/systeminfo/memory.d
@@ -1,13 +1,14 @@
 module systeminfo.memory;
 
-import std.conv : to;
-import std.file : readText;
-import std.string : chop, split, startsWith;
-
 ulong getTotalRAM()
 {
+    import std.conv : to;
+    import std.stdio : File;
+    import std.string : split, startsWith;
+
     ulong memTotal;
-    foreach (line; readText("/proc/meminfo").chop().split("\n"))
+
+    foreach (line; File("/proc/meminfo").byLine())
     {
         if (line.startsWith("MemTotal"))
             memTotal = line.split(" ")[$-2].to!ulong;
